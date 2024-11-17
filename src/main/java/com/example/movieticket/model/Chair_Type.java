c1545dcc8c339c6965eb5589cf88ef45a2cda286
kiethuynh-HK <115640911+kiethuynh-HK@users.noreply.github.com>
2024-11-18 06:39:24 +0700

    up

--- src/main/java/com/example/movieticket/model/Chair_Type.java
+++ src/main/java/com/example/movieticket/model/Chair_Type.java
@@ -3,6 +3,8 @@ package com.example.movieticket.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "chair_type")
@@ -23,6 +25,6 @@ public class Chair_Type {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="id_typechair")
     private  TypeChair typeChair;
-    @OneToOne(mappedBy = "chair_type",fetch = FetchType.LAZY)
-    private Status_Chair status_chair;
+    @OneToMany(mappedBy = "chair_type" , fetch = FetchType.LAZY)
+    private List<Status_Chair> status_chair;
 }
