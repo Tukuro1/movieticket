d5994e10ea0ceb7cd8202dcdd373fc7946760ae8
Nguyễn Tú <119604187+Tukuro1@users.noreply.github.com>
2024-11-23 00:34:30 +0700

    update_movie

--- src/main/java/com/example/movieticket/controller/AdminMovieController.java
+++ src/main/java/com/example/movieticket/controller/AdminMovieController.java
@@ -1,11 +1,7 @@
 package com.example.movieticket.controller;
 
 import com.example.movieticket.model.Movie;
-import com.example.movieticket.service.DirectorActorService;
 import com.example.movieticket.service.MovieService;
-
-import com.example.movieticket.service.RoomScheduDayService;
-import com.example.movieticket.service.TypeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,27 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/admin/movie")
 public class AdminMovieController {
     @Autowired
     private MovieService movieService;
-    @Autowired
-    private TypeService typeService;
-    @Autowired
-    private RoomScheduDayService roomScheduDayService;
-    @Autowired
-    private DirectorActorService directorActorService;
 
     @GetMapping
     public String showMovieList(Model model) {
+        List<Movie> movie = movieService.getAllMovies();
         model.addAttribute("movie", movieService.getAllMovies());
         return "movie/list";
     }
     @GetMapping("/add")
     public String addMovie(Model model) {
         model.addAttribute("movie", new Movie());
-        model.addAttribute("type", typeService.getAllTypes());
         return "/admin/movie/add";
     }
     @PostMapping("/add")
@@ -45,23 +38,29 @@ public class AdminMovieController {
         return "redirect:/admin/movie/list";
     }
     @GetMapping("/edit/{id}")
-    public String editMovie(@PathVariable Long id, Model model) {
-        Movie movie = movieService.getMovieById(id)
-                .orElseThrow(()-> new IllegalArgumentException("Invalid movie Id:" + id));
-        model.addAttribute("movie", movie);
-        model.addAttribute("type", typeService.getAllTypes());
-        model.addAttribute("schedu", roomScheduDayService.getAllRoomScheduDay());
-        model.addAttribute("director", directorActorService.getAllDirectorActor());
-        return "/admin/movie/edit";
+    public String editMovie(@PathVariable("id") Long id, Model model) {
+        Optional<Movie> editMovie=  movieService.getMovieById(id);
+        if(editMovie != null){
+            model.addAttribute("movie", editMovie);
+            return "admin/movie/edit";
+        }else {
+            return "not-found";
+        }
     }
     @PostMapping("/edit")
-    public String updateProduct(@PathVariable Long id, @Valid Movie movie, BindingResult result) {
-        if (result.hasErrors()) {
-            movie.setId(id); // set id to keep it in the form in case of errors
-            return "redirect:/admin/movie/edit";
+    public String updateProduct(@Valid @ModelAttribute("movie") Movie updateMovie, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("movie", movieService.getAllMovies());
+            return "admin/movie/edit";
         }
-        movieService.updateMovie(movie);
-        return "redirect:/admin/movie/list";
+        movieService.getAllMovies().stream()
+                .filter(movie -> movie.getClass() == updateMovie.getClass())
+                .findFirst()
+                .ifPresent( movie -> {
+
+                    movieService.updateMovie(updateMovie);
+                });
+        return "redirect:/admin/movie";
     }
     // Handle request to delete a product
     @GetMapping("/delete/{id}")
