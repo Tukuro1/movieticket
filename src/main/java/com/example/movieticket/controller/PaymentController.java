1fb10fac0f9d8e661f1efd8115c989d90c05642f
sang2.nguyen <sang2.nguyen@lge.com>
2024-12-02 02:55:06 +0000

    fix payment

--- src/main/java/com/example/movieticket/controller/PaymentController.java
+++ src/main/java/com/example/movieticket/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.example.movieticket.service.*;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -40,58 +41,64 @@ public class PaymentController {
 
         @GetMapping("/payment/save")
         @ResponseBody
-        public void paymentSave(@RequestParam Long scheduleId, @RequestParam(required = false) Long voucherCode,
+        public ResponseEntity<String>  paymentSave(@RequestParam Long scheduleId, @RequestParam(required = false) Long voucherCode,
                         @RequestParam List<Long> seatIds,
                         @AuthenticationPrincipal UserDetails userDetails) {
-                RoomSchedu_Time schedu_Time = roomScheduTimeService.findById(scheduleId)
-                                .orElseThrow(() -> new IllegalStateException("Schedule not found"));
-                List<Chair> chairs = chairService.getChairByIds(seatIds);
-                float total = schedu_Time.getTicketPrice() * chairs.size();
-                for (Chair chair : chairs) {
-                        if (chair.getChair_type() != null && chair.getChair_type().getPriceMore() != 0) {
-                                total += chair.getChair_type().getPriceMore();
+                try {
+                        RoomSchedu_Time schedu_Time = roomScheduTimeService.findById(scheduleId)
+                                        .orElseThrow(() -> new IllegalStateException("Schedule not found"));
+                        List<Chair> chairs = chairService.getChairByIds(seatIds);
+                        float total = schedu_Time.getTicketPrice() * chairs.size();
+                        for (Chair chair : chairs) {
+                                if (chair.getChair_type() != null && chair.getChair_type().getPriceMore() != 0) {
+                                        total += chair.getChair_type().getPriceMore();
+                                }
                         }
-                }
 
-                // Tạo và lưu Bill
-                Bill bill = new Bill();
-                bill.setDatebill(new Date()); // Ngày tạo hóa đơn
-                User user = this.userService.findByUsername(userDetails.getUsername())
-                                .orElseThrow(() -> new IllegalStateException("User not found"));
-                bill.setUser(user);
-                bill.setStatus(0); // 1: Đ ã thanh toán
-                bill.setTotalPrice(total);
-                if (voucherCode != null){
-                        Voucher voucher = voucherService.getVouvherById(voucherCode).orElse(null);
-                        if (voucher != null) {
-                                bill.setVoucher(voucher);
-                                voucher.setQuantity(voucher.getQuantity() - 1);
-                                bill.setTotalPrice(total - voucher.getDiscount() * total / 100);
+                        // Tạo và lưu Bill
+                        Bill bill = new Bill();
+                        bill.setDatebill(new Date()); // Ngày tạo hóa đơn
+                        User user = this.userService.findByUsername(userDetails.getUsername())
+                                        .orElseThrow(() -> new IllegalStateException("User not found"));
+                        bill.setUser(user);
+                        bill.setStatus(0); // 1: Đ ã thanh toán
+                        bill.setTotalPrice(total);
+                        if (voucherCode != null) {
+                                Voucher voucher = voucherService.getVouvherById(voucherCode).orElse(null);
+                                if (voucher != null) {
+                                        bill.setVoucher(voucher);
+                                        voucher.setQuantity(voucher.getQuantity() - 1);
+                                        bill.setTotalPrice(total - voucher.getDiscount() * total / 100);
+                                }
+                                voucherService.updateVoucher(voucher);
                         }
-                        voucherService.updateVoucher(voucher);
-                }
-                billService.save(bill); // Lưu Bill vào database
-                List<Status_Chair> statusChairs = new ArrayList<>();
-                List<Ticket> tickets = new ArrayList<>();
+                        billService.save(bill); // Lưu Bill vào database
+                        List<Status_Chair> statusChairs = new ArrayList<>();
+                        List<Ticket> tickets = new ArrayList<>();
 
-                // Tạo và lưu các Ticket tương ứng
-                for (Chair chair : chairs) {
-                        // Tạo mới Status_Chair
-                        Status_Chair newStatusChair = new Status_Chair();
-                        newStatusChair.setChair(chair);
-                        newStatusChair.setStatus("booked"); // Đặt ghế đã được đặt
-                        newStatusChair.setRoomschedu_time(schedu_Time);
-                        statusChairs.add(newStatusChair);
+                        // Tạo và lưu các Ticket tương ứng
+                        for (Chair chair : chairs) {
+                                // Tạo mới Status_Chair
+                                Status_Chair newStatusChair = new Status_Chair();
+                                newStatusChair.setChair(chair);
+                                newStatusChair.setStatus("booked"); // Đặt ghế đã được đặt
+                                newStatusChair.setRoomschedu_time(schedu_Time);
+                                statusChairs.add(newStatusChair);
 
-                        // Tạo Ticket mới
-                        Ticket ticket = new Ticket();
-                        ticket.setPrice(schedu_Time.getTicketPrice()
-                                        + (chair.getChair_type() != null ? chair.getChair_type().getPriceMore() : 0));
-                        ticket.setStatus_chair(newStatusChair);
-                        ticket.setBill(bill); // Liên kết ticket với hóa đơn
-                        tickets.add(ticket);
+                                // Tạo Ticket mới
+                                Ticket ticket = new Ticket();
+                                ticket.setPrice(schedu_Time.getTicketPrice()
+                                                + (chair.getChair_type() != null ? chair.getChair_type().getPriceMore()
+                                                                : 0));
+                                ticket.setStatus_chair(newStatusChair);
+                                ticket.setBill(bill); // Liên kết ticket với hóa đơn
+                                tickets.add(ticket);
+                        }
+                        this.status_chairService.addStatusChairs(statusChairs);
+                        this.ticketService.saveTickets(tickets);
+                        return ResponseEntity.ok("Thanh toán thành công");
+                } catch (Exception e) {
+                        return ResponseEntity.status(500).body("Thanh toán không thành công");
                 }
-                this.status_chairService.addStatusChairs(statusChairs);
-                this.ticketService.saveTickets(tickets);
         }
 }
