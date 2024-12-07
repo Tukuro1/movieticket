d66bc8d1d2dbd139cfca848ec60250264a850a0a
Nguyễn Tú <119604187+Tukuro1@users.noreply.github.com>
2024-12-07 23:57:13 +0700

    scdl02

--- src/main/java/com/example/movieticket/model/Room.java
+++ src/main/java/com/example/movieticket/model/Room.java
@@ -12,7 +12,7 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
-    @Column(name = "room_number", length = 10)
+    @Column(name = "room_number", length = 15)
     private String room_number;
     @Column(name = "row_count")
     private String row_count;
